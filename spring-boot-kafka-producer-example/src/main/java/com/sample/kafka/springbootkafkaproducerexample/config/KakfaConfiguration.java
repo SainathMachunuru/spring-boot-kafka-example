16f07903ca84775527e8a40e21e1771e10fc2a4f
Sainath Machunur <Sainath.Machunur1@T-Mobile.com>
2019-04-05 17:54:23 +0530

    first commit

---
+++ spring-boot-kafka-producer-example/src/main/java/com/sample/kafka/springbootkafkaproducerexample/config/KakfaConfiguration.java
@@ -0,0 +1,38 @@
+package com.sample.kafka.springbootkafkaproducerexample.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.sample.kafka.springbootkafkaproducerexample.model.User;
+
+@Configuration
+public class KakfaConfiguration {
+	 @Bean
+	    public ProducerFactory<String, User> producerFactory() {
+	        Map<String, Object> config = new HashMap<>();
+	        System.out.println("aaaaaaaaaaa");
+
+	        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+	        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+	        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+	        System.out.println("bbbbbbbbbb");
+
+	        return new DefaultKafkaProducerFactory<>(config);
+	    }
+
+
+	    @Bean
+	    public KafkaTemplate<String, User> kafkaTemplate() {
+	        return new KafkaTemplate<>(producerFactory());
+	    }
+
+
+}
